d8c19b52c07422d4bbd651254bc2df224b90806d
jimam <jimamtamimi12@gmail.com>
2022-08-27 11:12:16 +0600

    video 2 and 4 done

---
+++ 2 print comment/2_print_commet.py
@@ -0,0 +1,7 @@
+print('hello!!')
+print("jimam tamimi")
+
+"""
+print("jimam is a  good boy")
+print("jimam is a very good boy")
+"""
---
+++ 4 variables/variable.py
@@ -0,0 +1,65 @@
+# jimam = 34
+# # print(jimam)
+
+# a = 10
+# print(a)
+
+
+
+# x = 50
+# print(x)
+# article = ""
+# print(50)
+
+# a = 34
+# print(a)
+
+# ji = 100
+
+# print(ji)
+
+"""
+A variable name must start with a letter or the underscore character.
+A variable name cannot start with a number.
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ ).
+Variable names are case-sensitive (name, Name and NAME are three different variables).
+The reserved words(keywords) cannot be used naming the variable.
+"""
+
+# jimam = 20
+# _jimam = 20
+
+# jimam = 10
+# # 1jimam= 0 # wrong
+
+# # a-z, 0-9, _ 
+
+# jimam_tamimi = 10
+
+# age = 10
+# Age = 20
+# print(age)
+# print(Age)
+
+# jimam = 10
+# jImam = 20
+# print(jImam)
+
+# print , import , for, class 
+
+# a = 10
+# print(a)
+# a = 20
+# print(a)
+
+
+
+# a = 10
+# print(a)
+
+
+
+a = True
+print(a)
+
+
